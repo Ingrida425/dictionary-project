fa591fdcd5f63ec0fc47504444eafac7d54abb28
Ingrida425 <98320957+Ingrida425@users.noreply.github.com>
2022-10-25 07:48:29 +0200

    nothing special

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">There will be the Dictionary 📚</header>
+      <div className="container">
+        <header className="App-header">There will be the Dictionary 📚</header>
+        <footer className="text-center">Coded by Ingrida Kostiuk</footer>
+      </div>
     </div>
   );
 }
