82e92ef277953968aa9d8653e9d683c936c486f9
Ingrida425 <98320957+Ingrida425@users.noreply.github.com>
2022-11-11 12:34:13 +0100

    Photos API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,19 +2,30 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("sunshine");
   const [definitions, setDefinitions] = useState(null);
   const [loaded, setLoaded] = useState(false);
+  const [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data);
     setDefinitions(response.data[0]);
   }
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "563492ad6f917000010000019d453cb1917846e3bd2807ec419324df ";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -42,6 +53,7 @@ export default function Dictionary() {
           </form>
         </section>
         <Results results={definitions} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
